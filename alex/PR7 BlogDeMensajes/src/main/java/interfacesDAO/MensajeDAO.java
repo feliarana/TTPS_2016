a9fd7c1538eb1fe1c74905ab95f3b361669e4c98
Feli Arana <felipearana10@hotmail.com>
2016-11-11 17:22:33 -0300

    Dejo solo la entrega5

--- alex/PR7 BlogDeMensajes/src/main/java/interfacesDAO/MensajeDAO.java
+++
@@ -1,9 +0,0 @@
-package interfacesDAO;
-
-import java.util.List;
-import clases.Mensajes;
-
-public interface MensajeDAO {
-  public int agregarMensaje(String msj,int usuario);
-  public List<Mensajes> mostrarMensajes();
-}
--- alex/PR7 BlogDeMensajes/src/main/java/interfacesDAO/UsuarioDAO.java
+++
@@ -1,9 +0,0 @@
-package interfacesDAO;
-
-import clases.Usuario;
-
-public interface UsuarioDAO {//operaciones que implementan los usuarios
-
-	public Usuario encontrarUsuario(String contrasenia,String usuario);
-	
-}
